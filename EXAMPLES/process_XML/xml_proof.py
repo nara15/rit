87acd0893e4c994860c1c0acfed8c75a0955dc13
2013034348 Jose Mario Naranjo Leiva IC <jonaranjo@ec.tec.ac.cr>
2016-09-17 16:25:01 -0600

    glob xml files and index attributes

--- EXAMPLES/process_XML/xml_proof.py
+++ EXAMPLES/process_XML/xml_proof.py
@@ -1,7 +1,28 @@
 
+
+import xml.etree.ElementTree as ET
 import glob
 
-from xml.etree.ElementTree import ElementTree
+# Read XML from file
+url = 'C:/Users/jonaranjo/OneDrive/TEC/RIT/Flora-20160122/10-Quercus salicifolia Nee.xml'
+tree = ET.parse(url)
+# Getting the elements from XML
+ns='http://www.github.com/inbio'
+taxon_identification = tree.find("{%s}taxon_identification" %(ns)).attrib
+description = tree.find("{%s}description" %(ns)).attrib
+# Getting specific XML elements
+print(taxon_identification.get("rank"))
+print(taxon_identification.get("taxon_name"))
+print(description.get("taxon_description"))
 
 
-print(glob.glob("C:/Users/jonaranjo/OneDrive/TEC/RIT/Flora-20160122/*.xml"))
+
+# Tracing all xml in repository
+"""
+for x in glob.glob("C:/Users/jonaranjo/OneDrive/TEC/RIT/Flora-20160122/*.xml"):
+
+	tree = ET.parse(x)
+	root = tree.getroot()
+
+	print(root.tag)
+"""
