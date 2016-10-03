8440f234f1eaaa6467480d0334c9bb9a86b23633
Mario Naranjo <marionaranjo12@gmail.com>
2016-10-03 10:54:12 -0600

    first delivery completed

--- INDICE_INVERTIDO/project_source/search_structure.py
+++ INDICE_INVERTIDO/project_source/search_structure.py
@@ -11,25 +11,27 @@ This function improves vector space queries result by conditions over the gained
 
 """
 # Must receive path,ranking and query for improving ranking
-def structuredQueries(pRanking,pPrefijo, pRankingPath, pQueries = ""):
-
+def structuredQueries(pRanking,pPrefijo="TP1-", pRankingPath="", pQueries = ""):
+    #pRanking = [('478-Klaprothia fasciculata.xml', 0.1726670859381201), ('543-Calatola costaricensis.xml', 0.11084272481421198), ('547-Leretia cordata.xml', 0.10583586109558445), ('224-Magnolia sororum Seibert.xml', 0.10764146926445299), ('551-Oecopetalum greenmanii.xml', 0.17993655503379416), ('685-Lennoa madreporoides.xml', 0.07098280139850777), ('570-Lauraceae.xml', 0.049596695804410174), ('223-Magnolia poasana (Pittier).xml', 0.11303834637963545), ('209-Eschweilera calyculata Pittier.xml', 0.10556288449673286), ('691-Lennoa madreporoides.xml', 0.07098280139850777)]
     now = datetime.datetime.now()
-    
+
     # data is an html file with the query info
     data = "<!DOCTYPE html>\n<html>\n<body>\n"
     data = data + "<p>" + str(now) +"</p>"+"\n"
     data = data + "<p>" + pRankingPath +"</p>\n"
 
 
-
+    result = {}
+    resultAllQueries = {}
 
     ns='http://www.github.com/inbio'
 
     # queries
     #pQueries = "hojas arrangement espiraladas"
-    #pQueries = "ramitas pubescence glabras\nramitas coloration ennegrecidas"
+    #pQueries = "hojas arrangement espiraladas\nflores coloration blancas"
     #pQueries = "ramitas pubescence glabras"
     #pQueries = "ramitas pubescence glabras\nramitas pubescence glabras\nramitas pubescence glabras\nramitas pubescence glabras"
+
 
 
     pQueries = pQueries.split("\n")
@@ -45,12 +47,14 @@ def structuredQueries(pRanking,pPrefijo, pRankingPath, pQueries = ""):
     data = data + "</ul>\n"
     data = data + "<p>Result:</p>\n"
 
-    siCumplen = []
-    success = False
+    queriesNumber = len(pQueries)
+
     for pQuery in pQueries:
 
         for i in pRanking:
+            
             filePath = i[0]
+            #print("\n\n")
             tree = ET.parse(filePath)
             #finds taxon_identification element
             taxon_identification = tree.find("{%s}taxon_identification" %(ns)).attrib
@@ -70,49 +74,42 @@ def structuredQueries(pRanking,pPrefijo, pRankingPath, pQueries = ""):
                     biological_entity_name = biological_entity.attrib.get("name")
                     character_name = character.attrib.get("name")
                     character_value = character.attrib.get("value")
-                    #print(biological_entity_name +" "+ character_name +" "+ character_value)
-
 
                     if (pQuery[0] == biological_entity_name) and (pQuery[1] == character_name) and (pQuery[2] == character_value):
-                        #successCounter += 1
-                        #print(i)
 
-                        success = True;
-                        print(i)
-                        siCumplen.append(i)
+                        if result.get(i[0]) == None :
+                            #print("i[0]: " + i[0])
+                            #print("i[1]: " + str(i[1]))
+                            #print("\n")
+                            #i[0] name
+                            #i[1] weight
+                            result[i[0]] = i[1]
+                            resultAllQueries[i[0]] = 1
 
+                        else:
+                            #result[i[0]] += 1
+                            resultAllQueries[i[0]] += 1
 
-            if success == False:
-                """
-                data = data + "<p>" + str(success) +"<p/>"+"\n"
-                data = data + "</body>\n</html>"
-                print(data)
+    resultSorted = {}
+    #print (result)
+    for a in resultAllQueries:
+        #print("a: " + a + "result[a]: " + str(result[a]) )
+        if resultAllQueries[a] == queriesNumber :
+            print("This is a: " + a)
+            resultSorted[a] = a
 
+    sorted(resultSorted.values(),reverse=True)
 
-                f = open('result.html', 'w')
-                f.write(data)
-                f.close()
-                return success
-
-                """
-            else:
-                success = False
-
-    data = data + "<p>" + str(success) +"<p/>"+"\n"
-    if success == True:
-        data = data + "<ul\n"
-        for i in pRanking:
-            data = data + "<li>" + str(i) + "</li>\n"
-        data = data + "</ul>\n"
-    data = data + "<p>Archivos Exitosos</p>\n"
     data = data + "<ul>\n"
-    for i in siCumplen:
-        data = data + "<li>" + str(i) + "</li>\n"
-    data = data + "<ul>\n"
+    for a in resultSorted:
+        for b in pRanking:
+            if b[0] == a:
+                data = data +"<li>" + str(b) + "</li>\n"
+    data = data + "</ul>\n"
+
     data = data + "</body>\n</html>"
+    print(data)
 
     f = open(pPrefijo + "result.html", 'w')
     f.write(data)
     f.close()
-    return success
-
